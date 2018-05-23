3a2121a12a3f4a8b4f243ba866a86eda9053e993
upthagrg <upthagrg@oregonstate.edu>
2018-05-22 17:08:25 -0700

    added new working functions, need tests on old

--- cpulib.h
+++ cpulib.h
@@ -103,7 +103,7 @@ void int2hex(int temp, char* out, int idx){
 			exit(1);
 	}
 }
-
+/*
 char* bin2hex(char* bin, int offset){
 	int i=0; 
 	int length1 = strlen(bin);
@@ -121,4 +121,112 @@ char* bin2hex(char* bin, int offset){
 		int2hex((bin2int((), (4*i), 4)), temp, i);
 	}
 }
+*/
+/********************************************
+*Title: reverse
+*Input: char* string for input, char* string 
+*for output, int string size.
+*Description: reverses endianess of a string 
+*of hex values.
+********************************************/
+void reverse(char* in, char* out, int size){
+        for(int i = 0; i<size; i+=2){
+                out[size-1-i-1] = in[i];
+                out[size-1-i] = in[i+1];
+        }
+}
+
+/********************************************
+*Title: get_dec
+*Input: char
+*Ouput: int
+*Descrption: returns the decimal of a
+*hexidecimal represented as a char. 
+********************************************/
+int get_dec(char in){
+        if((int)in > 96){
+                in -= 32;
+        }
+        if(in == '0'){
+                return 0;
+        }
+        else if(in == '1'){
+                return 1;
+        }
+        else if(in == '2'){
+                return 2;
+        }
+        else if(in == '3'){
+                return 3;
+        }
+        else if(in == '4'){
+                return 4;
+        }
+        else if(in == '5'){
+                return 5;
+        }
+        else if(in == '6'){
+                return 6;
+        }
+        else if(in == '7'){
+                return 7;
+        }
+        else if(in == '8'){
+                return 8;
+        }
+        else if(in == '9'){
+                return 9;
+        }
+        else if(in == 'A'){
+                return 10;
+        }
+        else if(in == 'B'){
+                return 11;
+        }
+        else if(in == 'C'){
+                return 12;
+        }
+        else if(in == 'D'){
+                return 13;
+        }
+        else if(in == 'E'){
+                return 14;
+        }
+        else if(in == 'F'){
+                return 15;
+        }
+        else{
+                return -5;
+        }
+}
+
+/*******************************************
+*Title: convert_hex
+*Input: char* input string, int string size
+*Output: int
+*description: Converts a string 
+*representation of hexidecimal of length
+*decribed by size into an integer.
+*******************************************/
+int convert_hex(char* in, int size){
+        int i=0;
+        int j;
+        int pow;
+        int cur;
+        int total = 0;
+        for(i; i<size; i++){
+                j=0;
+                cur = 1;
+                pow = size-1-i;
+                for(j; j<pow; j++){
+                        cur *=16;
+                }
+                j=0;
+                for(j; j<(get_dec(in[i])); j++){
+                        total += (cur);
+                }
+        }
+        return total;
+}
+
 #endif
---
+++ detect_internet.c
@@ -0,0 +1,97 @@
+/*******************************************************
+*Title: detect_internet.h
+*Author: Glenn Upthagrove
+*Date: 01/30/18
+*Description: A simple test for internet connectivity
+*******************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h> //for strcmp
+
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+// multiprocessing includes 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+/***********************************************
+* Title: detect_internet_connection
+* Description: Detects if there is an internet 
+* connection present.
+***********************************************/
+int	detect_internet_connection(){
+	int devnul;
+	pid_t   childid;
+	int child_exit = -5;
+	int con;
+	int ret;
+
+	devnul = open("/dev/null", O_WRONLY);
+
+	childid = fork();
+
+        switch(childid){
+
+        case -1: //error
+                printf("ERROR SPAWNING CHILD\n");
+                fflush(stdout);
+                exit(2);
+
+        case 0: //child
+                //exec into wget
+		dup2(0, devnul); //duplicate /dev/null to stdin
+		dup2(1, devnul); //duplicate /dev/null to stdout
+		dup2(2, devnul); //duplicate /dev/null to stderr
+                ret = execlp("wget", "wget", "www.google.com", NULL);
+                if(ret == -1){
+			printf("ERROR CHECKING CONNECTION\n");
+			fflush(stdout);
+			exit(1);
+                }
+
+        default: //parent
+                //wait on wget
+                waitpid(childid, &child_exit, 0);
+		if(child_exit == 0){
+			con = 1;
+		}
+		else{
+			con = 0;
+		}
+        }
+	if(con == 1){ //clean up index.html
+		childid = fork();
+		        switch(childid){
+        
+	        case -1: //error
+	                printf("ERROR SPAWNING CHILD\n");
+	                fflush(stdout);
+	                exit(2);
+	        
+	        case 0: //child
+	                //exec into rm
+	                ret = execlp("rm", "rm", "./index.html", "-f", NULL);
+	                if(ret == -1){
+	                        printf("ERROR REMOVING INDEX.HTML\n");
+	                        fflush(stdout);
+	                        exit(1);
+	                }
+		        
+        	default: //parent
+        	        //wait on rm
+        	        waitpid(childid, &child_exit, 0);
+        	}
+	}
+	return con;
+}
+
+int main(){
+	return detect_internet_connection();
+}
